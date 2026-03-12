64bdd35ae4ae1a34e54b7408b50821ab3efbb661
Maiquelys Gonzalez <maiquelysgonzalez@gmail.com>
2026-03-12 19:45:39 -0400

    panel rosita

--- DeliveryMotoconcho/Delivery.UI/FrmMenu.Designer.cs
+++ DeliveryMotoconcho/Delivery.UI/FrmMenu.Designer.cs
@@ -13,80 +13,110 @@
 
         private void InitializeComponent()
         {
+            this.panelLateral = new System.Windows.Forms.Panel();
             this.btnRepartidores = new System.Windows.Forms.Button();
             this.btnPedidos = new System.Windows.Forms.Button();
             this.btnLiquidacion = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.labelSub = new System.Windows.Forms.Label();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.labelBienvenida = new System.Windows.Forms.Label();
             this.SuspendLayout();
 
-            // Fondo del formulario
-            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.panelLateral.Location = new System.Drawing.Point(0, 0);
+            this.panelLateral.Size = new System.Drawing.Size(200, 500);
+            this.panelLateral.BackColor = System.Drawing.Color.FromArgb(20, 20, 20);
+            this.panelLateral.Controls.Add(this.labelLogo);
+            this.panelLateral.Controls.Add(this.labelSub);
+            this.panelLateral.Controls.Add(this.btnRepartidores);
+            this.panelLateral.Controls.Add(this.btnPedidos);
+            this.panelLateral.Controls.Add(this.btnLiquidacion);
 
-            // Título
-            this.label1.Location = new System.Drawing.Point(80, 30);
-            this.label1.Text = "🛵 Sistema de Delivery";
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 18, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(255, 105, 180);
-            this.label1.Size = new System.Drawing.Size(340, 40);
-            this.label1.Name = "label1";
-            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.labelLogo.Text = "🛵";
+            this.labelLogo.Font = new System.Drawing.Font("Segoe UI", 36);
+            this.labelLogo.ForeColor = System.Drawing.Color.FromArgb(255, 105, 180);
+            this.labelLogo.Size = new System.Drawing.Size(180, 70);
+            this.labelLogo.Location = new System.Drawing.Point(10, 20);
+            this.labelLogo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelLogo.BackColor = System.Drawing.Color.Transparent;
 
-            // Botón Repartidores
-            this.btnRepartidores.Location = new System.Drawing.Point(100, 100);
-            this.btnRepartidores.Text = "👤  Gestión de Repartidores";
+            this.labelSub.Text = "Delivery\nMotoconcho";
+            this.labelSub.Font = new System.Drawing.Font("Segoe UI", 11, System.Drawing.FontStyle.Bold);
+            this.labelSub.ForeColor = System.Drawing.Color.White;
+            this.labelSub.Size = new System.Drawing.Size(180, 50);
+            this.labelSub.Location = new System.Drawing.Point(10, 95);
+            this.labelSub.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSub.BackColor = System.Drawing.Color.Transparent;
+
+            this.btnRepartidores.Text = "👤  Repartidores";
             this.btnRepartidores.Name = "btnRepartidores";
-            this.btnRepartidores.Size = new System.Drawing.Size(250, 50);
+            this.btnRepartidores.Size = new System.Drawing.Size(200, 50);
+            this.btnRepartidores.Location = new System.Drawing.Point(0, 180);
             this.btnRepartidores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRepartidores.BackColor = System.Drawing.Color.FromArgb(255, 105, 180);
+            this.btnRepartidores.BackColor = System.Drawing.Color.FromArgb(20, 20, 20);
             this.btnRepartidores.ForeColor = System.Drawing.Color.White;
-            this.btnRepartidores.Font = new System.Drawing.Font("Segoe UI", 10, System.Drawing.FontStyle.Bold);
+            this.btnRepartidores.Font = new System.Drawing.Font("Segoe UI", 10);
             this.btnRepartidores.FlatAppearance.BorderSize = 0;
+            this.btnRepartidores.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRepartidores.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
             this.btnRepartidores.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnRepartidores.Click += new System.EventHandler(this.btnRepartidores_Click);
 
-            // Botón Pedidos
-            this.btnPedidos.Location = new System.Drawing.Point(100, 170);
-            this.btnPedidos.Text = "📦  Gestión de Pedidos";
+            this.btnPedidos.Text = "📦  Pedidos";
             this.btnPedidos.Name = "btnPedidos";
-            this.btnPedidos.Size = new System.Drawing.Size(250, 50);
+            this.btnPedidos.Size = new System.Drawing.Size(200, 50);
+            this.btnPedidos.Location = new System.Drawing.Point(0, 235);
             this.btnPedidos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPedidos.BackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.btnPedidos.ForeColor = System.Drawing.Color.FromArgb(255, 105, 180);
-            this.btnPedidos.Font = new System.Drawing.Font("Segoe UI", 10, System.Drawing.FontStyle.Bold);
-            this.btnPedidos.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(255, 105, 180);
-            this.btnPedidos.FlatAppearance.BorderSize = 1;
+            this.btnPedidos.BackColor = System.Drawing.Color.FromArgb(20, 20, 20);
+            this.btnPedidos.ForeColor = System.Drawing.Color.White;
+            this.btnPedidos.Font = new System.Drawing.Font("Segoe UI", 10);
+            this.btnPedidos.FlatAppearance.BorderSize = 0;
+            this.btnPedidos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPedidos.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
             this.btnPedidos.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPedidos.Click += new System.EventHandler(this.btnPedidos_Click);
 
-            // Botón Liquidación
-            this.btnLiquidacion.Location = new System.Drawing.Point(100, 240);
-            this.btnLiquidacion.Text = "💰  Liquidación Diaria";
+            this.btnLiquidacion.Text = "💰  Liquidación";
             this.btnLiquidacion.Name = "btnLiquidacion";
-            this.btnLiquidacion.Size = new System.Drawing.Size(250, 50);
+            this.btnLiquidacion.Size = new System.Drawing.Size(200, 50);
+            this.btnLiquidacion.Location = new System.Drawing.Point(0, 290);
             this.btnLiquidacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLiquidacion.BackColor = System.Drawing.Color.FromArgb(60, 60, 60);
-            this.btnLiquidacion.ForeColor = System.Drawing.Color.FromArgb(255, 105, 180);
-            this.btnLiquidacion.Font = new System.Drawing.Font("Segoe UI", 10, System.Drawing.FontStyle.Bold);
-            this.btnLiquidacion.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(255, 105, 180);
-            this.btnLiquidacion.FlatAppearance.BorderSize = 1;
+            this.btnLiquidacion.BackColor = System.Drawing.Color.FromArgb(20, 20, 20);
+            this.btnLiquidacion.ForeColor = System.Drawing.Color.White;
+            this.btnLiquidacion.Font = new System.Drawing.Font("Segoe UI", 10);
+            this.btnLiquidacion.FlatAppearance.BorderSize = 0;
+            this.btnLiquidacion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLiquidacion.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
             this.btnLiquidacion.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnLiquidacion.Click += new System.EventHandler(this.btnLiquidacion_Click);
 
-            // Form
-            this.ClientSize = new System.Drawing.Size(450, 340);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnRepartidores);
-            this.Controls.Add(this.btnPedidos);
-            this.Controls.Add(this.btnLiquidacion);
-            this.Text = "Delivery Motoconcho";
+            this.panelContenido.Location = new System.Drawing.Point(200, 0);
+            this.panelContenido.Size = new System.Drawing.Size(600, 500);
+            this.panelContenido.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+            this.panelContenido.Controls.Add(this.labelBienvenida);
+
+            this.labelBienvenida.Text = "🛵 Bienvenida al Sistema\nde Delivery Motoconcho\n\nSelecciona una opción del menú.";
+            this.labelBienvenida.Font = new System.Drawing.Font("Segoe UI", 14, System.Drawing.FontStyle.Bold);
+            this.labelBienvenida.ForeColor = System.Drawing.Color.FromArgb(255, 105, 180);
+            this.labelBienvenida.Size = new System.Drawing.Size(560, 200);
+            this.labelBienvenida.Location = new System.Drawing.Point(20, 150);
+            this.labelBienvenida.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelBienvenida.BackColor = System.Drawing.Color.Transparent;
+
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.panelLateral);
+            this.Controls.Add(this.panelContenido);
+            this.Text = "Sistema Delivery Motoconcho";
+            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
             this.Name = "FrmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
         }
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelLateral;
+        private System.Windows.Forms.Panel panelContenido;
         private System.Windows.Forms.Button btnRepartidores;
         private System.Windows.Forms.Button btnPedidos;
         private System.Windows.Forms.Button btnLiquidacion;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.Label labelSub;
+        private System.Windows.Forms.Label labelBienvenida;
     }
 }
--- DeliveryMotoconcho/Delivery.UI/FrmMenu.cs
+++ DeliveryMotoconcho/Delivery.UI/FrmMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Delivery.UI
@@ -8,24 +9,50 @@ namespace Delivery.UI
         public FrmMenu()
         {
             InitializeComponent();
+            btnRepartidores.Click += btnRepartidores_Click;
+            btnPedidos.Click += btnPedidos_Click;
+            btnLiquidacion.Click += btnLiquidacion_Click;
+        }
+
+        private void ActivarBoton(Button btn)
+        {
+            btnRepartidores.BackColor = Color.FromArgb(20, 20, 20);
+            btnRepartidores.ForeColor = Color.White;
+            btnPedidos.BackColor = Color.FromArgb(20, 20, 20);
+            btnPedidos.ForeColor = Color.White;
+            btnLiquidacion.BackColor = Color.FromArgb(20, 20, 20);
+            btnLiquidacion.ForeColor = Color.White;
+
+            btn.BackColor = Color.FromArgb(255, 105, 180);
+            btn.ForeColor = Color.White;
+        }
+
+        private void AbrirFormulario(Form frm)
+        {
+            panelContenido.Controls.Clear();
+            frm.TopLevel = false;
+            frm.FormBorderStyle = FormBorderStyle.None;
+            frm.Dock = DockStyle.Fill;
+            panelContenido.Controls.Add(frm);
+            frm.Show();
         }
 
         private void btnRepartidores_Click(object sender, EventArgs e)
         {
-            FrmRepartidor frm = new FrmRepartidor();
-            frm.ShowDialog();
+            ActivarBoton(btnRepartidores);
+            AbrirFormulario(new FrmRepartidor());
         }
 
         private void btnPedidos_Click(object sender, EventArgs e)
         {
-            FrmPedido frm = new FrmPedido();
-            frm.ShowDialog();
+            ActivarBoton(btnPedidos);
+            AbrirFormulario(new FrmPedido());
         }
 
         private void btnLiquidacion_Click(object sender, EventArgs e)
         {
-            FrmLiquidacion frm = new FrmLiquidacion();
-            frm.ShowDialog();
+            ActivarBoton(btnLiquidacion);
+            AbrirFormulario(new FrmLiquidacion());
         }
     }
 }
